de10ebea505c252e0ca18c1f083172aede794e0d
Neil Dwyer <neildwyer1991@gmail.com>
2021-11-14 13:01:10 -0800

    full loop working

--- Assets/ThirdWeb/ThirdWeb.cs
+++ Assets/ThirdWeb/ThirdWeb.cs
@@ -2,19 +2,45 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class ThirdWeb : MonoBehaviour
 {
-    public void CheckLoggedIn()
+    private Dictionary<string, TaskCompletionSource<string>> taskMap = new Dictionary<string, TaskCompletionSource<string>>();
+
+    [System.Serializable]
+    private struct GenericMessage
+    {
+        public string ack_id;
+    }
+
+    [System.Serializable]
+    private struct CheckLoggedInMsg
+    {
+        public bool result;
+    }
+
+    public async void CheckLoggedIn()
     {
         var ack_id = ThirdWebInvoke("thirdweb.logged_in", "{}");
-        Debug.LogFormat("ack_id: {0}", ack_id);
+        var tr = new TaskCompletionSource<string>();
+        taskMap[ack_id] = tr;
+        var msg = await tr.Task;
+        var loggedInMsg = JsonUtility.FromJson<CheckLoggedInMsg>(msg);
+        Debug.LogFormat("logged in message: {0}", msg);
+        Debug.LogFormat("logged in: {0}", loggedInMsg.result);
     }
 
     public void Callback(string payload)
     {
-        Debug.LogFormat("Callback called: {0}", payload);
+        var msg = JsonUtility.FromJson<GenericMessage>(payload);
+        if(taskMap.ContainsKey(msg.ack_id))
+        {
+            var tr = taskMap[msg.ack_id];
+            tr.SetResult(payload);
+            taskMap.Remove(msg.ack_id);
+        }
     }
 
     [DllImport("__Internal")]
